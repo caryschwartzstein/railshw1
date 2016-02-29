2ebb0e7272492ade0fa0cf5dff0b33d447159d69
cary.schwartzstein <cary.schwartzstein@berkeley.edu>
2016-02-28 17:53:22 -0800

    hw1 finished

--- fa15-hw1/foobar.rb
+++ fa15-hw1/foobar.rb
@@ -1,3 +1,23 @@
 class Foobar
   # Q4 CODE HERE
+ #  In hw1.rb we defined a function called blockin_time that requires the foobar.rb file,
+ #  and then calls a class method 'baz' from the Foobar class.
+	# If you take a look at the foobar.rb, you'll see that we've defined Foobar. 
+	# Write a class function in foobar.rb called baz that takes in an array of integers as strings, 
+	# changes each string into an integer, adds two to each number, keeps the even numbers only, takes out any duplicates, 
+	# rejects the resulting numbers greater than ten, and returns the sum of the resulting array.
+	# def intitialize(a)
+	# 	@a = a
+	# end
+	def self.baz(a)
+		# a = a.sort
+		b = 0
+		for i in a
+			i = i.to_i
+			if i < 10 && i % 2 == 0
+				b += i + 2
+			end
+		end
+		return b
+	end
 end
--- fa15-hw1/hw1.rb
+++ fa15-hw1/hw1.rb
@@ -5,10 +5,19 @@ end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
+  a = a.sort
+  l = 0
+  while l < a.length
+  	a[l] = a[l] + 1
+  	l = l + 1
+  end
+  return a
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
+   return "#{first_name} #{last_name}"
+
 end
 
 def blockin_time(a)
